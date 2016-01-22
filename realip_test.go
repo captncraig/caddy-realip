95d935a4a7f41b1b34051f42c4b221e7f74d2d88
Craig Peterson <cpeterson@stackoverflow.com>
2016-01-22 16:46:18 -0700

    adding tests

--- realip_test.go
+++ realip_test.go
@@ -16,12 +16,13 @@ func TestRealIP(t *testing.T) {
 		path       string
 		expectedIP string
 	}{
-		{"1.2.3.4", "", "foo", "1.2.3.4"},
-		{"4.4.255.255", "", "xyz", "4.4.255.255"},
-		{"4.5.0.0", "1.2.3.4", "xyz", "1.2.3.4"},
-		{"4.5.2.3", "1.2.6.7,5.6.7.8,111.111.111.111", "xyz", "1.2.6.7"},
-		{"4.5.5.5", "NOTANIP", "xyz", "4.5.5.5"},
+		{"1.2.3.4:123", "", "foo", "1.2.3.4:123"},
+		{"4.4.255.255:123", "", "xyz", "4.4.255.255:123"},
+		{"4.5.0.0:123", "1.2.3.4", "xyz", "1.2.3.4:123"},
+		{"4.5.2.3:123", "1.2.6.7,5.6.7.8,111.111.111.111", "xyz", "1.2.6.7:123"},
+		{"4.5.5.5:123", "NOTANIP", "xyz", "4.5.5.5:123"},
 		{"aaaaaa", "1.2.3.4", "xyz", "aaaaaa"},
+		{"aaaaaa:123", "1.2.3.4", "xyz", "aaaaaa:123"},
 	} {
 		remoteAddr := ""
 		_, ipnet, err := net.ParseCIDR("4.5.0.0/16") // "4.5.x.x"
@@ -42,7 +43,7 @@ func TestRealIP(t *testing.T) {
 		if err != nil {
 			t.Fatalf("Test %d: Could not create HTTP request: %v", i, err)
 		}
-		req.RemoteAddr = test.actualIP + ":34567"
+		req.RemoteAddr = test.actualIP
 		if test.headerVal != "" {
 			req.Header.Set("X-Real-IP", test.headerVal)
 		}
@@ -50,7 +51,7 @@ func TestRealIP(t *testing.T) {
 		rec := httptest.NewRecorder()
 		he.ServeHTTP(rec, req)
 
-		if remoteAddr != test.expectedIP+":34567" {
+		if remoteAddr != test.expectedIP {
 			t.Errorf("Test %d: Expected '%s', but found '%s'", i, test.expectedIP, remoteAddr)
 		}
 	}
