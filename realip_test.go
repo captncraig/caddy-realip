2e14fda8c22d252eca734d981ec89e4ee7e25ff2
Kelsey Price <kelsey@raven.ws>
2019-07-10 08:53:42 -0500

    Correct caddy import path. Issue #17

--- realip_test.go
+++ realip_test.go
@@ -8,8 +8,8 @@ import (
 
 	"bytes"
 	"fmt"
-	"github.com/mholt/caddy"
-	"github.com/mholt/caddy/caddyhttp/httpserver"
+	"github.com/caddyserver/caddy"
+	"github.com/caddyserver/caddy/caddyhttp/httpserver"
 )
 
 func TestRealIP(t *testing.T) {
